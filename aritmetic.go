a9065ca01a71ffaef1739ca4561c613c7e0e1657
Christian <3003html@gmail.com>
2022-06-04 20:23:48 -0500

    Aritmetic

---
+++ aritmetic.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	x := 5
+	y := 15
+	// Addition
+	result := x + y
+	fmt.Println("Addition: ", result)
+	// Subtraction
+	result = y - x
+	fmt.Println("Subtraction: ", result)
+	// Multiplication
+	result = x * y
+	fmt.Println("Multiplication: ", result)
+	// Division
+	result = y / x
+	fmt.Println("Division: ", result)
+	// Module
+	result = y % x
+	fmt.Println("Module: ", result)
+	// Incremental
+	x++
+	fmt.Println(x)
+	// Decremental
+	x--
+	fmt.Println(x)
+	// Circle area
+	var r int = 10
+	areaC := math.Pow(float64(r), 2) * math.Pi
+	fmt.Println("Area: ", areaC)
+	// Trapezium area
+	a := 10
+	b := 12
+	areaT := (a + b) / 2
+	fmt.Println(areaT)
+}
--- hello_world.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	//Hellow World
-	fmt.Println("Hello World")
-}
