40a60fac25cad5895e1d578dae17020f018cf28b
Christian <3003html@gmail.com>
2022-06-04 19:50:39 -0500

    Variables

--- hello_world.go
+++ hello_world.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
+	//Hellow World
 	fmt.Println("Hello World")
 }
---
+++ variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println()
+	//Constants
+	const pi = 3.14
+	fmt.Println(pi)
+	//Variable
+	var base int = 10
+	fmt.Println(base)
+	pow := base * base
+	fmt.Println(pow)
+	//Zero Values
+	var a int
+	var b float64
+	var c string
+	var d bool
+	fmt.Println(a, b, c, d)
+}
