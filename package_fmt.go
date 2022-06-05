9ba065a1a260bad173e888e196efa12f7f475654
Christian <3003html@gmail.com>
2022-06-05 13:25:05 -0500

    Package fmt

--- package_fmt.go
+++ package_fmt.go
@@ -8,4 +8,14 @@ func main() {
 	integ := 10
 	// Line break
 	fmt.Println("Linea break", chain, integ)
+	// Template String
+	fmt.Printf("String: %s\nInt: %d", chain, integ)
+	// Template String for variables
+	fmt.Printf("\nVariable: %v", chain)
+	// Sprintf
+	message := fmt.Sprintf("New message: %d", integ)
+	fmt.Println()
+	fmt.Println(message)
+	// Datatype
+	fmt.Printf("Datatype of integ variable: %T", integ)
 }
