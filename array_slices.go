12fa27105c5762c282769b9bbbadf8f1c38ec6bc
Christian <3003html@gmail.com>
2022-06-05 21:35:04 -0500

    slices and arrays

---
+++ array_slices.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Arrays
+	array := [6]int{0, 1, 2, 3, 4, 5}
+	fmt.Println(array)
+	fmt.Println(len(array), cap(array))
+	// Slice
+	slice := []int{0, 1, 2, 3}
+	fmt.Println(slice)
+	slice2 := slice[:2]
+	fmt.Println(slice2, len(slice2))
+	slice = append(slice, 4)
+	fmt.Println(slice)
+	slice3 := []int{5, 6}
+	slice = append(slice, slice3...)
+	fmt.Println(slice)
+	slice[0] = 10
+	fmt.Println(slice, slice2)
+}
